38932c4d0b9dfa9514bcd712dbb4a0eec9a61cc8
chobo0206 <dust0206@daum.net>
2023-07-02 02:49:58 +0900

    test2

--- src/Seoul.java
+++ src/Seoul.java
@@ -2,5 +2,8 @@
 public class Seoul {
 	public static void main(String[] args) {
 		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
 	}
 }
